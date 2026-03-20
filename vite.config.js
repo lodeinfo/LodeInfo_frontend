e033c43239098f1f773f1b90af5fef5880242e27
lodeinfo <shalin@lodeinfo.ai>
2026-03-20 17:00:35 +0530

    Build fix 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.icns"],
+  assetsInclude: ["**/*.ico"],
   base: "./", // Important for Electron to load assets correctly in production
   resolve: {
     alias: {
