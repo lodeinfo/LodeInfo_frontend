99d416ecfbd0258335c5e5f086e27cee031fbe8f
lodeinfo <shalin@lodeinfo.ai>
2026-03-20 17:38:56 +0530

    Build fix 7

--- main.cjs
+++ main.cjs
@@ -20,7 +20,7 @@ function createWindow() {
     icon: iconPath,
     skipTaskbar: false,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, "preload.cjs"),
       contextIsolation: true,
       nodeIntegration: false,
     },
